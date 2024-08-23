53d7b54273a8dbaaae848143fc0ea7ec3e06f434
mdjonayed6 <jonayed2640@gmail.com>
2024-08-23 14:16:58 +0600

    Logic problems solved and finalize the project

--- script/app.js
+++ script/app.js
@@ -10,49 +10,62 @@ for (const btn of allBtn) {
       "selected-players-container"
     );
     // disabled cart
-    e.target.setAttribute("disabled", true);
+    var buttonColor = true;
+    //e.target.setAttribute("disabled", true);
     e.target.parentNode.style.backgroundColor = "gray";
 
-    const countDown = getTargetValue("cart");
-    if (countDown + 1 > 6) {
-      alert("Your team is ready. You can't take more player");
-      e.target.setAttribute("disabled", false);
-      e.target.parentNode.style.backgroundColor = "#010313";
-      return;
-    }
-
-    // budget, cart and remaining updated from here
-    const budget = getTargetValue("budget");
-    updateBudget = budget - parseInt(price);
-    document.getElementById("budget").innerText = updateBudget;
-
     // cart update
-    const cartUpdate = getTargetValue("cart");
-    document.getElementById("cart").innerText = cartUpdate + 1;
-
+    const cartValue = getTargetValue("cart");
     // Remaining Players
     const remainingPlayers = getTargetValue("remaining");
-    document.getElementById("remaining").innerText = remainingPlayers - 1;
 
     // Create a div with the <p> to keep the player details
     const div = document.createElement("div");
     div.classList.add("flex", "justify-around", "p-2");
 
-    const p1 = document.createElement("p");
-    const p2 = document.createElement("p");
-    const p3 = document.createElement("p");
-    p1.innerText = name;
-    p2.innerText = price;
-    p3.innerText = category;
+    // budget, cart and remaining updated from here
+    const budget = getTargetValue("budget");
+    updateBudget = budget - parseInt(price);
 
-    div.appendChild(p1);
-    div.appendChild(p2);
-    div.appendChild(p3);
+    if (updateBudget < 0) {
+      //over budget
+      alert("over budget");
+      buttonColor = false;
+      //e.target.setAttribute("disabled", buttonColor);
+      e.target.parentNode.style.backgroundColor = "#010313";
+    } else {
+      //within budget
+      document.getElementById("budget").innerText = updateBudget;
+      document.getElementById("cart").innerText = cartValue + 1;
+      document.getElementById("remaining").innerText = remainingPlayers - 1;
+      AddToWishlist(name, price, category, div, selectedPlayersContainer);
 
-    selectedPlayersContainer.appendChild(div);
-    updateTotalCost(price);
-    updateGrandTotal();
+      updateTotalCost(price);
+      updateGrandTotal();
+    }
+
+    e.target.disabled = buttonColor;
+
+    if (cartValue > 6) {
+      alert("Your team is ready. You can't take more player");
+      return;
+    }
   });
+}
+
+function AddToWishlist(name, price, category, div, selectedPlayersContainer) {
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+  const p3 = document.createElement("p");
+  p1.innerText = name;
+  p2.innerText = price;
+  p3.innerText = category;
+
+  div.appendChild(p1);
+  div.appendChild(p2);
+  div.appendChild(p3);
+
+  selectedPlayersContainer.appendChild(div);
 }
 
 // calculate Total cost
